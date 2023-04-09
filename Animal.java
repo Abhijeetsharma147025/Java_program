0f8255d1009c68f7b343bd204a83f77c794b2010
abhijeet kumar <abhisharmaku099@gmail.com>
2023-04-09 18:51:22 +0530

    second commit

---
+++ Animal.java
@@ -0,0 +1,18 @@
+class Animal
+{
+public void eat()
+{
+System.out.println("i am eating");
+}
+public void run()
+{
+System.out.println("i am running");
+}
+public static void main(String args[])
+{
+System.out.println("1");
+Animal word=new Animal();
+word.eat();
+word.run();
+}
+} 
